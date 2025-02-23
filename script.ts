3ab7509ac6366913e3f1bd5b08227e17e2cb3202
MatiasCardoso4 <cardosomatias1994@gmail.com>
2025-02-23 18:10:44 -0300

    Implement product display feature with dynamic loading from JSON and update HTML structure for improved layout

---
+++ script.ts
@@ -0,0 +1,48 @@
+const products = document.querySelector('.hero-products-list')
+
+
+async function getProducts() {
+  try {
+    const response = await fetch("./homeProducts/homeProducts.json");
+    const data:string[] = await response.json();
+    setDisplay(data)
+  } catch (e:any) {
+    console.error(e.message);
+  }
+}
+
+
+function setDisplay(data:string[]){
+  data.forEach((product:any) => {
+    createElements(product)
+  });
+}
+
+function createElements(product:any){
+const card = document.createElement('div')
+card.classList.add('hero-products-card')
+const img = document.createElement('img')
+img.classList.add('hero-products-card-img')
+const title = document.createElement('h3')
+title.classList.add('hero-products-card-title')
+const price = document.createElement('p')
+price.classList.add('hero-products-card-price')
+const brand = document.createElement('p')
+brand.classList.add('hero-products-card-brand')
+const button = document.createElement('button')
+
+const buttonContainer = document.createElement('div')
+buttonContainer.classList.add('hero-products-card-button-container')
+buttonContainer.append(button)
+
+button.textContent = 'Agregar al carrito'
+button.classList.add('hero-products-card-button')
+img.src = product.image
+title.textContent = product.name
+price.textContent = product.price
+brand.textContent = product.brand
+card.append(img,title,brand,price, buttonContainer)
+products?.append(card)
+}
+
+getProducts()
